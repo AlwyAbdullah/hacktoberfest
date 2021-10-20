62a2693c0eae6fa001ba20874d943d9d17eb288f
AlwyAbdullah <54932526+AlwyAbdullah@users.noreply.github.com>
2021-10-20 23:49:01 +0700

    Add Basic Code Igniter Code

---
+++ application/views/layout/header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Penelitian 2021</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Google Api -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Favicons -->
+    <link href="<?= base_url() ?>assets/img/favicon.png" rel="icon">
+    <link href="<?= base_url() ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="<?= base_url() ?>assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top header-inner-pages">
+        <div class="container d-flex align-items-center">
+
+            <h1 class="logo"><a href="index.html">Pnltian</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+            <nav class="nav-menu d-none d-lg-block">
+
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+
+                    <!-- Example dropdown -->
+                    <!-- <li class="drop-down"><a href="#">About</a>
+                        <ul>
+                            <li class="active"><a href="about.html">About Us</a></li>
+                            <li><a href="team.html">Team</a></li>
+                            <li><a href="testimonials.html">Testimonials</a></li>
+
+                            <li class="drop-down"><a href="#">Deep Drop Down</a>
+                                <ul>
+                                    <li><a href="#">Deep Drop Down 1</a></li>
+                                    <li><a href="#">Deep Drop Down 2</a></li>
+                                    <li><a href="#">Deep Drop Down 3</a></li>
+                                    <li><a href="#">Deep Drop Down 4</a></li>
+                                    <li><a href="#">Deep Drop Down 5</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li> -->
+
+                    <li><a href="#">Profil Pasar</a></li>
+                    <li><a href="#">Riwayat Harga</a></li>
+                    <li><a href="#">Rekomendasi</a></li>
+
+                </ul>
+
+            </nav><!-- .nav-menu -->
+
+            <a href="index.html" class="get-started-btn ml-auto">Get Started</a>
+
+        </div>
+    </header><!-- End Header -->
+
+    <main id="main">
